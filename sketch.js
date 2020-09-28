981c4115721cfb0df944e9640ea1d6612c145077
Adityaverma135 <72012186+Adityaverma135@users.noreply.github.com>
2020-09-28 20:00:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,7 +103,7 @@ function draw() {
     }
     
     //jump when the space key is pressed
-    if(keyDown("space") && trex.y >= 100) {
+    if(keyDown(space) && trex.y >= 100) {
         trex.velocityY = -12;
         jumpSound.play();
     }
